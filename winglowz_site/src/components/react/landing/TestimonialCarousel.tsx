0e7d30c7a4758837410784c92589678f318f9a45
Diane Defores <35034946+dianedef@users.noreply.github.com>
2026-07-17 13:51:00 +0000

    up

--- winglowz_site/src/components/react/landing/TestimonialCarousel.tsx
+++ winglowz_site/src/components/react/landing/TestimonialCarousel.tsx
@@ -5,7 +5,7 @@ export const testimonials = [
   {
     name: "Florin Muresan",
     role: "CEO & Co-Founder at Squirrly",
-    avatarSrc: "https://winglowz.b-cdn.net/Florin_muresan.jpg",
+    avatarSrc: "https://winflowz.b-cdn.net/Florin_muresan.jpg",
     title: "One of the Best Deals I Purchased All Year!",
     quote:
       "I just spent 20 minutes in it and I'm already wondering what I've been doing with my life until now :)) Lots of goodies to improve my work. I love it! I thought I was being productive and that my setup was good and adapted for speed... but I guess I was wrong. There's a lot to go through and it's so well organized. I feel like I've discovered hidden treasure.",
@@ -15,7 +15,7 @@ export const testimonials = [
   {
     name: "Alex",
     role: "Verified Purchaser",
-    avatarSrc: "https://winglowz.b-cdn.net/alex-dynapictures.png",
+    avatarSrc: "https://winflowz.b-cdn.net/alex-dynapictures.png",
     title: "Actionable Advice and Profound Market Research",
     quote:
       "This product contains lots of useful information, obviously the authors are experts in market analysis and productivity tools. I like the idea of working smarter, not harder, and one can achieve this by applying the right tools for the job. Nice, they even covered how to stay focused, what tools to use to eliminate distractions and not to procrastinate!",
